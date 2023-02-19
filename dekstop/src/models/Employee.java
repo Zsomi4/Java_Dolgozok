733f425b12422cb5204ef5c77b40f2c9f08ca277
Zsomi4 <k72302872172@ktch.hu>
2023-02-19 13:46:18 +0100

    Dolgozok adattagjainak publikusá tétele és a server inditó script megírása

--- dekstop/src/models/Employee.java
+++ dekstop/src/models/Employee.java
@@ -2,38 +2,8 @@ package models;
 
 public class Employee {
 
-    String city;
-    String address;
-    Integer size;
-    Double price;
-    public Employee(String city, String address, Integer size, Double price) {
-        this.city = city;
-        this.address = address;
-        this.size = size;
-        this.price = price;
-    }
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    public Integer getSize() {
-        return size;
-    }
-    public void setSize(Integer size) {
-        this.size = size;
-    }
-    public Double getPrice() {
-        return price;
-    }
-    public void setPrice(Double price) {
-        this.price = price;
-    }
+    public String city;
+    public String address;
+    public Integer size;
+    public Double price;
 }
